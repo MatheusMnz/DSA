18234a3f7dbbaf06fed7be2e958282c814b76d03
MatheusMenezes <matheus.menezes0806@gmail.com>
2024-09-25 13:53:15 -0300

    Deleting Method using another aproach

--- part2/bstTree.py
+++ part2/bstTree.py
@@ -117,13 +117,13 @@ class BinaryTreeNode:
 
         return elements
     
-    def delete(self, value):
+    def delete_min_rightsubtree(self, value):
         if value < self.data:
             if self.left:
-                self.left = self.left.delete(value)
+                self.left = self.left.delete_min_rightsubtree(value)
         elif value > self.data:
             if self.right:
-                self.right = self.right.delete(value)
+                self.right = self.right.delete_min_rightsubtree(value)
         else:
             # Caso o nó a ser deletado tenha 0 ou 1 filho
             if self.left is None:
@@ -136,8 +136,35 @@ class BinaryTreeNode:
             min_val = self.right.min()
             self.data = min_val
             # Deleta o nó que tinha o menor valor na subárvore direita
-            self.right = self.right.delete(min_val)
+            self.right = self.right.delete_min_rightsubtree(min_val)
             
+        return self
+    
+    def delete_max_leftsubtree(self, value):
+        # Procuro o Nó na subárvore esquerda com recursão
+        if value < self.data:
+            if self.left:
+                self.left = self.left.delete_max_leftsubtree(value)
+        # Procuro o nó na subárvore direita com recursão
+        elif value > self.data:
+            if self.right:
+                self.right = self.right.delete_max_leftsubtree(value)
+        # Nó encontrado
+        else:
+            # Se tiver um filho retorna o filho e atualiza o ponteiro na pilha de recursão para o nó pai
+            # Se não tiver filho retorna None por default
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.left
+            
+            # Pego o maior valor da subárvore esquerda
+            max_val = self.left.max()
+            # Gero uma cóṕia do valor
+            self.data = max_val
+            # Removo a duplicidade da subárvore esquerda
+            self.left = self.left.delete_max_leftsubtree(max_val)
+
         return self
 
             
@@ -159,5 +186,9 @@ if __name__ == '__main__':
     print(f'Pre_Order: {numbers_tree.pre_order_traversal()}')
     print(f'Valor mínimo: {numbers_tree.min()}')
     print(f'Valor máximo: {numbers_tree.max()}')
-    numbers_tree.delete(9)
+    numbers_tree.delete_min_rightsubtree(9)
     print(f'In_Order: {numbers_tree.in_order_traversal()}')
+    numbers_tree = build_tree(numbers)
+    numbers_tree.delete_max_leftsubtree(9)
+    print(f'In_Order: {numbers_tree.in_order_traversal()}')
+
